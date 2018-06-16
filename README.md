a95d8fa50c6386439d3b0d224535289142408896
Idakwoji Theophilus <pietrosparks@gmail.com>
2018-06-16 23:45:03 +0100

    removing untracked files

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # Bears-Team-23
-Add-project-description-here | Voyage-5 | chingu.io
+Blablaclone | Voyage-5 | chingu.io
+
+##Installation
+
+_Must have NodeJS, npm and MongoDB installed before proceeding_
+
+> 1. Clone the repository
+2. Navigate to **env.example** file and create an env file out of it. Replacing database name as required
+3. Run _npm install_
+4. Run _npm start_
+
+
+### Extra Notes
+
+> All Backend logic and setup for the database is contained in **_api_** folder while the rest of the folders on the _root_ are client related (VueJS)
+
+
---
+++ api/utils.js
@@ -0,0 +1,57 @@
+const randomID = require('random-id');
+const JWT = require('jsonwebtoken');
+const secrets = require('./dbconfig/secrets');
+const bcrypt = require('bcrypt-nodejs');
+const salt = bcrypt.genSaltSync(10);
+
+const encryptPayload = (payload) => {
+
+  return JWT.sign({
+    data: payload,
+    exp: Math.floor(Date.now() / 1000) + 360,
+    iat: Math.floor(new Date(Date.now()))
+  }, secrets.JWT_SECRET)
+
+}
+
+const requestAuthorization = (req, res, next) => {
+
+  let bearerToken;
+  let bearerHeader = req.headers.authorization;
+
+  if (typeof bearerHeader !== 'undefined') {
+
+    bearerToken = bearerHeader.split(' ')[1]
+    req.token = bearerToken;
+
+    JWT.verify(bearerToken, secrets.JWT_SECRET, (err, verified) => {
+
+      if (err) {
+          return res.sendStatus(403);
+      }
+      if (verified) {
+        return next();
+      }
+    })
+  } else return res.sendStatus(403)
+}
+
+const passwordHash = (password) => {
+
+  let salt = bcrypt.genSaltSync(10);
+  let encrypted = bcrypt.hashSync(password, salt);
+  return encrypted;
+}
+
+const passwordDecrypt = (password, hashedPassword) => {
+  return bcrypt.compareSync(password, hashedPassword);
+}
+
+
+module.exports = {
+  randomID: randomID,
+  hasher: passwordHash,
+  decrypter: passwordDecrypt,
+  encryptPayload: encryptPayload,
+  requestAuthorization: requestAuthorization,
+}
