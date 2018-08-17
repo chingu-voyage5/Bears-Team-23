7cf3cc58e5a65b492ed7004ae7b9c38d65884b5b
Theo <theo@Theos-MacBook-Air.local>
2018-08-17 12:52:00 +0100

    change

--- seed.js
+++ seed.js
@@ -1,5 +1,5 @@
 module.exports = {
 	"undefined": "localhost/database",
 	"dev": "localhost/database",
-	"production": "mongodb://bearsteam23:bearsteam23@ds117101.mlab.com:17101/bearsteam2"
+	"production": "mongodb://bearsteam23:bearsteam23@ds117101.mlab.com:17101/bearsteam23"
 }
